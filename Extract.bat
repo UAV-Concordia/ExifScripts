56799f0fd106549b642e7829bdffd5e710649e0f
pǝɯɐɥoɯ <mo7_abd@hotmail.com>
2015-04-26 08:57:59 -0700

    remove Header and take files from images folder

--- Extract.bat
+++ Extract.bat
@@ -1,1 +1,3 @@
-exiftool -csv -r  -gpslatitude -gpslongitude ./ > output.csv
+exiftool -csv -r  -gpslatitude -gpslongitude ./images > ListOfImages.csv
+more +1 "ListOfImages.csv" >"ListOfImages.csv.new"
+move /y "ListOfImages.csv.new" "ListOfImages.csv"
